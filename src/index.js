94cec59d1fccaaba622d6258ad09957b5c359ed2
sukumar Vaddi <svaddi_July_1_2015@macbook-pro.attlocal.net>
2020-03-11 13:32:15 -0500

    Asset - Loading CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import './style.css';
+
 
 function component() {
     const element = document.createElement('div');
-
+    element.classList.add('hello')
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
