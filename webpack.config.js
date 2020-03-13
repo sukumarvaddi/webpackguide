2cbea3ed7cb5a00a6891a548a00a0412c028d1df
sukumar Vaddi <svaddi_July_1_2015@macbook-pro.attlocal.net>
2020-03-13 10:33:30 -0500

    Code Splitting - Prevent Duplication

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,19 @@ module.exports = {
     mode: 'development',
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        another: './src/another-module.js'
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-        }),
-    ],
+
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
 
-        // Used with webpack-dev-middle ware in conjuction with express server. Check server.js to see how it is used.
-        publicPath: '/'
     },
 
-
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
 
 };
