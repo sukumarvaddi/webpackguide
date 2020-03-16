939623528f47e630539c369450557eb3af81b1a1
sukumar Vaddi <svaddi_July_1_2015@macbook-pro.attlocal.net>
2020-03-16 10:27:39 -0500

    AuthoringLibrary

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,20 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Caching',
-        }),
-    ],
+    mode: 'production',
     output: {
-        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'webpack-numbers.js',
+        library: 'webpacknumbers',
+        libraryTarget: 'umd'
     },
-    optimization: {
-        moduleIds: 'hashed',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_',
         },
     },
 };
